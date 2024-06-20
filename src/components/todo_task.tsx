a62af50eb1e465d546b932c880fefe1b0e41e0a8
Smitha <smitha.kotian@gmail.com>
2024-06-20 19:36:32 +0100

    add logic to edit a task description from the list

--- src/components/todo_task.tsx
+++ src/components/todo_task.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import { Task } from "../types/task.types";
+import ErrorMessage from "./error_message";
+import validateDescription from "./validate/validate_description";
 
 export interface ToDoTaskProps {
   task: Task;
   onDelete: (toDeleteTask: Task) => void;
 }
 const ToDoTask: React.FC<ToDoTaskProps> = ({ task, onDelete }) => {
+  const [description, setDescription] = useState<string>(task.description);
   const [isChecked, setChecked] = useState<boolean>(task.isComplete);
+  const [editable, setEditable] = useState<boolean>(false);
 
   const handleCheckbox = async () => {
     task.isComplete = !isChecked;
@@ -25,6 +29,37 @@ const ToDoTask: React.FC<ToDoTaskProps> = ({ task, onDelete }) => {
         await response.json();
       } else {
         alert("error");
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  const onDescriptionChange = (description: string) => {
+    setDescription(description);
+  };
+
+  const toggleEditable = () => {
+    setEditable(!editable);
+  };
+
+  const editDone = async () => {
+    toggleEditable();
+    task.description = description;
+
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(task),
+    };
+
+    try {
+      const url = "http://localhost:8888/tasks/" + task.id;
+      const response = await fetch(url, requestOptions);
+
+      if (response.status === 200) {
+        await response.json();
+      } else {
+        alert("error fetching tasks");
       }
     } catch (e) {
       console.error(e);
@@ -56,7 +91,25 @@ const ToDoTask: React.FC<ToDoTaskProps> = ({ task, onDelete }) => {
   return (
     <>
       <tr>
-        <td>{task.description}</td>
+        <td>
+          <input
+            type="text"
+            id="description"
+            name="description"
+            placeholder={description}
+            disabled={!editable}
+            onChange={(event) => onDescriptionChange(event.target.value)}
+          ></input>
+          <ErrorMessage messages={validateDescription(description)} />
+          {editable && (
+            <button
+              disabled={!(validateDescription(description).length === 0)}
+              onClick={editDone}
+            >
+              Done
+            </button>
+          )}
+        </td>
         <td>
           <input
             type="checkbox"
@@ -68,6 +121,11 @@ const ToDoTask: React.FC<ToDoTaskProps> = ({ task, onDelete }) => {
         </td>
         <td>
           <button onClick={handleDelete}>Delete</button>
+        </td>
+        <td>
+          <button disabled={editable} onClick={toggleEditable}>
+            Edit
+          </button>
         </td>
       </tr>
     </>
